4abe042e81b7a49d6434d81fbe2acd3d30829018
Arsh Bhatia <arshbhatia2001@gmail.com>
2023-06-12 13:24:50 -0400

    fixed functions a little bit

--- molecool/functions.py
+++ molecool/functions.py
@@ -5,6 +5,27 @@ import matplotlib.pyplot as plt
 
 from mpl_toolkits.mplot3d import Axes3D
 
+def canvas(with_attribution=True):
+    """
+    Placeholder function to show example docstring (NumPy format).
+
+    Replace this function and doc string for your own.
+
+    Parameters
+    ----------
+    with_attribution : bool, Optional, default: True
+        Set whether or not to display who the quote is from.
+
+    Returns
+    -------
+    quote : str
+        Compiled string including quote and optional attribution.
+    """
+
+    quote = "The code is but a canvas."
+    if with_attribution:
+        quote += "\n\t- Adapted from Henry David Thoreau"
+    return quote
 
 def calculate_distance(rA, rB):
     # This function calculates the distance between two points given as numpy arrays.
@@ -167,25 +188,4 @@ if __name__ == "__main__":
 
 
 
-def canvas(with_attribution=True):
-    """
-    Placeholder function to show example docstring (NumPy format).
-
-    Replace this function and doc string for your own.
-
-    Parameters
-    ----------
-    with_attribution : bool, Optional, default: True
-        Set whether or not to display who the quote is from.
-
-    Returns
-    -------
-    quote : str
-        Compiled string including quote and optional attribution.
-    """
-
-    quote = "The code is but a canvas."
-    if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
-    return quote
 
